33adfefbd01a8869493c9689627b4febcca78a92
belutooth <lostman.fikri@gmail.com>
2016-05-05 12:54:52 +0700

    yo

---
+++ flaskr/flaskrs.py
@@ -0,0 +1,86 @@
+from flask import Flask, session, redirect, url_for, escape, request, render_template, jsonify
+from hashlib import md5
+from base64 import b64encode
+import MySQLdb
+
+app = Flask(__name__)
+
+db = MySQLdb.connect(host="localhost", user="root", passwd="", db="tcoverflow")
+cur = db.cursor()
+
+@app.route('/')
+def index():
+    error = None
+    print index
+    if 'username' in session:
+        return redirect(url_for('user'))
+    return render_template('index.html', error=error)
+
+@app.route('/signup/login', methods=['GET', 'POST'])
+def login():
+    print login
+    error = None
+    if 'username' in session:
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        username_form  = request.form['username']
+        password_form  = request.form['password']
+        cur.execute("SELECT COUNT(1) FROM users WHERE username = %s;", [username_form]) # CHECKS IF USERNAME EXSIST
+        if cur.fetchone()[0]:
+            cur.execute("SELECT password FROM users WHERE username = %s;", [username_form]) # FETCH THE HASHED PASSWORD
+            for row in cur.fetchall():
+                if password_form == row[0]:
+                    session['username'] = request.form['username']
+                    return redirect(url_for('user'))
+                else:
+                    error = "Salah password!"
+        else:
+            error = "Anda belum terdaftar!"
+    return render_template('sign_up.html', error=error)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    print signup
+    error = None
+    if 'username' in session:
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        username_form  = request.form['username']
+        password_form  = request.form['password']
+        cur.execute("SELECT COUNT(1) FROM users WHERE username = %s;", [username_form]) # CHECKS IF USERNAME EXSIST
+        if cur.fetchone()[0]:
+            error = "Username sudah digunakan!"
+        else:
+            cur.execute("INSERT INTO users(username, password) VALUES(%s ,%s)", ([username_form],[password_form]))
+            db.commit()
+            error = "Berhasil Daftar!"
+            
+    return render_template('sign_up.html', error=error)
+
+@app.route('/user', methods=['GET', 'POST'])
+def user():
+    if 'username' in session:
+        username_session = session['username']
+        return render_template('user.html', session_user_name=username_session)
+    return redirect(url_for('index'))
+
+@app.route('/user/<id>', methods=['GET', 'POST'])
+def user_byid(id):
+    if 'username' in session:
+        cur.execute("SELECT * FROM users where username = %s",[id])
+        passingData = []
+        passingData.append(cur.fetchall())
+        passingData.append('data')
+        return render_template('profil.html', data=passingData)
+    return redirect(url_for('index'))
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+#app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+app.secret_key = 'awankinton123'
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ flaskr/tcoverflow.sql
@@ -0,0 +1,130 @@
+/*
+SQLyog Ultimate v9.02 
+MySQL - 5.5.5-10.0.17-MariaDB : Database - tcoverflow
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`tcoverflow` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `tcoverflow`;
+
+/*Table structure for table `jawaban` */
+
+DROP TABLE IF EXISTS `jawaban`;
+
+CREATE TABLE `jawaban` (
+  `id_jawaban` int(11) NOT NULL AUTO_INCREMENT,
+  `id_soal` int(11) NOT NULL,
+  `rating_jawaban` int(11) NOT NULL DEFAULT '0',
+  `isi_jawaban` text NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `tgl_jawaban` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_jawaban`),
+  KEY `id_soal` (`id_soal`),
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `jawaban_ibfk_1` FOREIGN KEY (`id_soal`) REFERENCES `question` (`id_question`),
+  CONSTRAINT `jawaban_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `jawaban` */
+
+/*Table structure for table `picture` */
+
+DROP TABLE IF EXISTS `picture`;
+
+CREATE TABLE `picture` (
+  `id_picture` int(11) NOT NULL AUTO_INCREMENT,
+  `isi_picture` longblob NOT NULL,
+  `id_question` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_picture`),
+  KEY `FK_picture` (`id_question`),
+  CONSTRAINT `FK_picture` FOREIGN KEY (`id_question`) REFERENCES `question` (`id_question`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `picture` */
+
+/*Table structure for table `question` */
+
+DROP TABLE IF EXISTS `question`;
+
+CREATE TABLE `question` (
+  `id_question` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `pertanyaan` text NOT NULL,
+  `judul` varchar(50) NOT NULL,
+  `tgl_question` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_question`),
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `question_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `question` */
+
+/*Table structure for table `tags` */
+
+DROP TABLE IF EXISTS `tags`;
+
+CREATE TABLE `tags` (
+  `id_tags` int(11) NOT NULL AUTO_INCREMENT,
+  `C_tags` int(11) NOT NULL DEFAULT '0',
+  `C++_tags` int(11) NOT NULL DEFAULT '0',
+  `C#_tags` int(11) NOT NULL DEFAULT '0',
+  `HTML_tags` int(11) NOT NULL DEFAULT '0',
+  `PHP_tags` int(11) NOT NULL DEFAULT '0',
+  `JS_tags` int(11) NOT NULL DEFAULT '0',
+  `Py_tags` int(11) NOT NULL DEFAULT '0',
+  `VB_tags` int(11) NOT NULL DEFAULT '0',
+  `Bash_tags` int(11) NOT NULL DEFAULT '0',
+  `Java_tags` int(11) NOT NULL DEFAULT '0',
+  `Android_tags` int(11) NOT NULL DEFAULT '0',
+  `Unity_tags` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_tags`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `tags` */
+
+/*Table structure for table `tagstoquestion` */
+
+DROP TABLE IF EXISTS `tagstoquestion`;
+
+CREATE TABLE `tagstoquestion` (
+  `id_tagstoquestion` int(11) NOT NULL AUTO_INCREMENT,
+  `id_tags` int(11) NOT NULL,
+  `id_question` int(11) NOT NULL,
+  PRIMARY KEY (`id_tagstoquestion`),
+  KEY `id_tags` (`id_tags`),
+  KEY `id_question` (`id_question`),
+  CONSTRAINT `tagstoquestion_ibfk_1` FOREIGN KEY (`id_tags`) REFERENCES `tags` (`id_tags`),
+  CONSTRAINT `tagstoquestion_ibfk_2` FOREIGN KEY (`id_question`) REFERENCES `question` (`id_question`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `tagstoquestion` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(40) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `status` int(11) NOT NULL,
+  `foto_user` longblob,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
